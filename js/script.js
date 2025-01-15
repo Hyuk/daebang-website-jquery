0bc961bc0d39e5c92b4e99e45bf0e4c1283b1cab
Hyuk <hyukho83@gmail.com>
2025-01-16 08:11:38 +0900

    Global Navigation Bar 마우스 오버시 보일 수 있도록 이펙트 추가

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.gnb > li > a').on('mouseenter focus', function () {
+    var gnbindex = $('.gnb > li > a').index($(this));
+    $('.gnb .inner').removeClass('on'); //
+    $('.gnb .inner:eq(' + gnbindex + ')').addClass('on');
+  });
+  $('header').on('mouseleave', function () {
+    $('.gnb .inner').removeClass('on');
+  });
+});
