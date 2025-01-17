85b161f326ffe989bda31f201d0fca5476e67a7c
Hyuk <hyukho83@gmail.com>
2025-01-18 08:18:45 +0900

    상단 이미지 슬라이드 배너의 인디케이터 스타일 추가

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ $(function () {
 $(function () {
   $('.visual .slide').slick({
     arrow: true, // 화살표
-    dots: false, // 인디케이터
+    dots: true, // 인디케이터
     autoplay: true, // 자동재생
     fade: true, // 페이드인 효과
     autoplaySpeed: 7000, // 재생시간
